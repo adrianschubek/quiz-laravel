1abfce231c4a9cc241dedf641082d333cb088253
adrian <adrian.schubek@gmail.com>
2019-12-17 23:31:29 +0100

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,8 @@
 <main>
     @yield('content')
 </main>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319" preserveAspectRatio="none" style="display: inherit"
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319" preserveAspectRatio="none"
+     style="display: inherit; overflow: hidden"
      height="60px" width="100%">
     <path fill="#273036" fill-opacity="1"
           d="M0,288L40,266.7C80,245,160,203,240,197.3C320,192,400,224,480,229.3C560,235,640,213,720,202.7C800,192,880,192,960,213.3C1040,235,1120,277,1200,266.7C1280,256,1360,192,1400,160L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
