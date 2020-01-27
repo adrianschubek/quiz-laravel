621c3d316be3af674192094814a52153ea53724e
adrian <adrian.schubek@gmail.com>
2020-01-27 20:23:55 +0100

    update

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -12,29 +12,6 @@ class QuestionPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any questions.
-     *
-     * @param User $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the question.
-     *
-     * @param User $user
-     * @param Question $question
-     * @return mixed
-     */
-    public function view(User $user, Question $question)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can create questions.
      *
      * @param User $user
@@ -44,53 +21,5 @@ class QuestionPolicy
     public function create(User $user, Quiz $quiz)
     {
         return $user === $quiz->user;
-    }
-
-    /**
-     * Determine whether the user can update the question.
-     *
-     * @param User $user
-     * @param Question $question
-     * @return mixed
-     */
-    public function update(User $user, Question $question)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the question.
-     *
-     * @param User $user
-     * @param Question $question
-     * @return mixed
-     */
-    public function delete(User $user, Question $question)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the question.
-     *
-     * @param User $user
-     * @param Question $question
-     * @return mixed
-     */
-    public function restore(User $user, Question $question)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the question.
-     *
-     * @param User $user
-     * @param Question $question
-     * @return mixed
-     */
-    public function forceDelete(User $user, Question $question)
-    {
-        //
     }
 }
